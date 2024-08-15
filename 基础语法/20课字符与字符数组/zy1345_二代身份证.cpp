8bfc7c8e3c3420a0aba581ec3b932bbcd18f1bd6
fengjiansu <c2306547791@gmail.com>
2024-08-15 14:15:43 +0800

    20课代码&&更改mingw作为编辑器

---
+++ 基础语法/20课字符与字符数组/zy1345_二代身份证.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char s[100];
+int main()
+{
+   cin>>s;
+   if(s[16]%2)
+   {
+    cout<<'M';
+   }
+   else
+   {
+    cout<<'W';
+   }
+   return 0;
+}
